9d0bafb5da26db0aa16f67c02076f683cc03e408
martinbustos <martinfederico.bustos@mercadolibre.com>
2020-04-30 20:51:31 -0300

    MallardDuckTest - duckShouldBeAMallardTest added.

---
+++ src/test/java/com/ing_soft/strategy/ducks/MallardDuckTest.java
@@ -0,0 +1,35 @@
+package com.ing_soft.strategy.ducks;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MallardDuckTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void duckShouldBeAMallard() {
+		MallardDuck mallard = new MallardDuck();
+		assertEquals("I'm a real Mallard duck", mallard.display());
+	}
+
+}
